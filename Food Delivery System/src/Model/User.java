fe9344297f26c1029a3372f8789fe7a2b50b9e35
Tomi <tomi.vd02@gmail.com>
2024-01-12 14:49:17 +0200

    Salt

--- Food Delivery System/src/Model/User.java
+++ Food Delivery System/src/Model/User.java
@@ -8,14 +8,16 @@ public class User {
     private String address;
     private String phoneNumber;
     private String role;
+    private byte[] salt;
 
-    public User(String name, String email, String hashedPassword, String address, String phoneNumber, String role) {
+    public User(String name, String email, String hashedPassword, String address, String phoneNumber, String role,byte[] salt) {
         this.name = name;
         this.email = email;
         this.hashedPassword = hashedPassword;
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.role = role;
+        this.salt = salt;
     }
 
     public int getUser_id() {
@@ -72,5 +74,13 @@ public class User {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
     }
 }
